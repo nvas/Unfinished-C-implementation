ab2dde6cb77ef66ff3c6f95241c1ad8048d4d4e8
nvas <sajidqurashi1@gmail.com>
2016-10-19 04:58:11 +0100

    Update MessageEncryption.cpp

--- New Project-20161013/MessageEncryption.cpp
+++ New Project-20161013/MessageEncryption.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-    /*  Declaring message,keys and counter  */
+    /*  Declaring message,keys and keys pointers and counter  */
     string msg;
-    int key_enc,key_dec,i;
-    string temp;
+    int *key_enc_ptr,key_enc,*key_dec_ptr,key_dec,i;
+    key_enc_ptr=&key_enc;
+    key_dec_ptr=&key_dec;
+    string cryptography,*cryptography_ptr;
+    
     cout<<"Enter msg"<<endl;
     getline(cin,msg);
     /*  Getting user message  */
@@ -15,19 +18,21 @@ int main()
     cin>>key_enc;
     
     
-
-    temp  = msg[i] + key_enc;
-    
     /*  Encrypting user message by casting message to integer according to ASCII table */
+    
+    cryptography  = msg[i] + key_enc;           //casting msg to integer and add added to key value
+    cryptography_ptr = &cryptography;           //Referencing
+    
+    
     cout<<"This is your encrypted message"<<endl;
     for ( i=0;i<=msg.length();i++)
-        cout<<temp;
+        cout<<cryptography_ptr;
         cout<<endl;
     
     /*  Decrypting user message  */
     cout<<"Enter Decryption key"<<endl;
     cin>>key_dec;
-    if(key_enc == key_dec){
+    if(*key_enc_ptr == *key_dec_ptr){       
         cout<<msg<<endl;
     }else
         cout<<"sorry"<<endl;
