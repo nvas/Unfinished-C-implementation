4299d17bd416e56db9fe5c6e20bf6dd2328198a4
Sajid Fadlelseed <sajidqurashi1@gmail.com>
2016-10-23 12:15:27 +0100

    Update vector1.cpp

--- vector1.cpp
+++ vector1.cpp
@@ -1,45 +1,51 @@
-#include <iostream>
+#include <iostream> 
 #include<vector>
 using namespace std;
-void showall(vector<int> vec); 
+void showall(vector<int> MyVec);
 int main()
 {
-        int num, index;
-        vector<int> my_vector;
-        do {
-            cout<<"Enter a number: "<<endl; 
-            cin>>num;
-                if (num!=-1)
-                    my_vector.push_back(num);
-
-        }   while (num!=-1);
-
-                    cout<<"You entered "<<my_vector.size()<< " numbers."<<endl; 
-                    cout<<"Enter an index: "<<endl;
-                    cin>>index;
-                    cout<<"At index "<<index<<": "<<my_vector[index]<<endl; 
-                    cout<<"You vector values: ";
-                    showall(my_vector);
-                    cout<<"Index of the item you want to remove: "<<endl; 
-                    cin>>index;
-                    my_vector.erase(my_vector.begin()+index);
-                    cout<<"You vector values: ";
-                    showall(my_vector);
-                    cout<<"Now you have "<<my_vector.size()<<"numbers."<<endl; 
-                    return 0;
+  
+    int price;
+    int index;
+    vector<int> MyVector;
+    
+    cout<<"Enter your price \n";
+  
+    do{
+        
+        cin>>price;
+        if (price != -1)
+        MyVector.push_back(price);
+      
+   
+    }while(price !=-1);
+    
+    cout<<"You Enetered "<<MyVector.size()<<" Prices "<<" for "<<MyVector.size()<<" products \n";
+    cout<<"Enter an index \n";
+    cin>>index;
+    cout<<" at Index "<<index<<" : "<<MyVector[index]<<endl;
+    cout<<"Your vector prices values are : ";
+    showall(MyVector);
+    cout<<"Enter the index of price you want to remove \n";
+    cin>>index;
+    MyVector.erase(MyVector.begin()+index);
+    cout<<"Now vector values are \n";
+    showall(MyVector);
+    cout<<endl;
+    cout<<" Numbers of prices are "<<MyVector.size()<<endl;
+    
+    
+    return 0;
     }
 
+void showall(vector<int> MyVec){
 
-void showall(vector<int> vec) 
-{
-for(vector<int>::iterator item =vec.begin(); item!=vec.end();item++) 
+    for(vector<int>::iterator item=MyVec.begin();item != MyVec.end();item++)
+    {
+                cout<<*item<<" ";
+        
+    }
+                 cout<<endl;
 
-cout<<*item<<' ';
 
-cout<<endl;
 }
-
-
-
-
-
