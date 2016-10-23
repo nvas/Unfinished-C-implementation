42798314729aac8b0277b20617d1a5c60e675127
Sajid Fadlelseed <sajidqurashi1@gmail.com>
2016-10-23 17:54:32 +0100

    Update IO_functions.cpp

--- IO_functions.cpp
+++ IO_functions.cpp
@@ -4,18 +4,26 @@
 using namespace std;
 
 void input(string question,int *var);
-int age;
+void print(int number);
+void print(string text);
+void print(double doubleN);
+void println(char x);
+
 int main (){
-
-  
-
+    
+ int age;
+    
     input("Please Enter your Age",&age);
 
-    cout<<endl;
-
-
+    
+    print(6);
+    println('X');
+    print("Any text  ");
+    print(123.234);
+    
+    
     return 0;
-
+    
 }
 
 void input(string question,int *var){
@@ -24,6 +32,32 @@ void input(string question,int *var){
     cin>>*var;
     
     cout<<"you entered \n"<<*var<<endl;
+    
+    
+}
+void print(int number){
+    
+    cout<<number;
 
 
 }
+void println(char x){
+    
+    cout<<x<<"\n";
+    
+    
+}
+void print(string text){
+    
+    cout<<text;
+    
+    
+}
+void print(double doubleN){
+    
+    cout<<doubleN;
+    cout<<endl;
+    
+    
+}
+
