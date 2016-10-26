029ecda5565ddee9ea3b09b95f660f5fcf1c1982
Sajid Fadlelseed <sajidqurashi1@gmail.com>
2016-10-26 19:17:33 +0100

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+
+        int num;
+        int i;
+        int fact=1;
+        cin>>num;
+      for( i=1;i<=num;i++)
+        fact=i*fact;
+        cout<<fact<<"\n";
+   
+  
+
+ return 0;
+}
